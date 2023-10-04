14bf4be0894d7e306ffb13d48a520727cd03b1a4
Chuan <kirk336neo@gmail.com>
2023-10-04 09:06:44 +0800

    feat ($ compile): Add airflow crawler log
    - use loguru to log airflow crawler

--- crawler/dags/crawling_ptt_testing.py
+++ crawler/dags/crawling_ptt_testing.py
@@ -2,7 +2,7 @@ from dag_crawling import *
 
 base_url = "https://www.ptt.cc/bbs/HatePolitics/index.html"
 ptt_board = "gossip" if "Gossiping" in base_url else "politics"
-for i in range(1, 2):
+for i in range(2, 3):
     crawl_results = crawl_articles(base_url, i, 1)
     if crawl_results:
         collection = db[ptt_board]
